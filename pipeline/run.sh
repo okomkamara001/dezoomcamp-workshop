ebc1500d6db21b0d69ab1abe887c07f5f9b6985d
okomkamara001 <okamara@idtlabs.xyz>
2026-01-12 14:23:17 +0000

    Docker compose of the pipeline working

--- pipeline/run.sh
+++ pipeline/run.sh
@@ -49,3 +49,21 @@ docker run -it \
     --year=2021 \
     --month=2 \
     --chunksize=100000
+
+# check the network link:
+docker network ls 
+
+# it's pipeline_default
+# now run the script:
+docker run -it \
+  --network=pipeline_default \
+  taxi_ingest:v001 \
+    --pg-user=root \
+    --pg-pass=root \
+    --pg-host=pgdatabase \
+    --pg-port=5432 \
+    --pg-db=ny_taxi \
+    --target-table=yellow_taxi_trips_2021_1 \
+    --year=2021 \
+    --month=1 \
+    --chunksize=100000
